218e434f89a2fd5558e6374cc653321aec179ff7
Tiri <me@tiri2.dev>
2024-06-12 12:22:53 +0200

    setter, getter & commandHandler added

--- chat/src/main/java/com/ryifestudios/twitch/Chat.java
+++ chat/src/main/java/com/ryifestudios/twitch/Chat.java
@@ -1,17 +1,23 @@
 package com.ryifestudios.twitch;
 
 
+import com.ryifestudios.twitch.commands.CommandHandler;
 import com.ryifestudios.twitch.configuration.Configuration;
+import lombok.Getter;
 
 public class Chat {
 
-    WSClient client;
+    @Getter
+    private final WSClient client;
+
+    @Getter
+    private final CommandHandler commandHandler;
 
     public Chat(Configuration config, ChatAuthentication authentication) {
 
+        this.commandHandler = new CommandHandler();
+
         client = new WSClient(config, authentication);
-
-
 
     }
 
