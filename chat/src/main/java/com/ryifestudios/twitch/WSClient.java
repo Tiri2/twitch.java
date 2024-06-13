07b54e529d007747240582289e0f48880dbdef2a
BS Schule <Juergen.Soukup@students.bs-linz2.ac.at>
2024-06-13 11:09:44 +0200

    code clean up

--- chat/src/main/java/com/ryifestudios/twitch/WSClient.java
+++ chat/src/main/java/com/ryifestudios/twitch/WSClient.java
@@ -3,6 +3,8 @@ package com.ryifestudios.twitch;
 import com.ryifestudios.twitch.commands.CommandHandler;
 import com.ryifestudios.twitch.configuration.Configuration;
 import com.ryifestudios.twitch.parser.IRCMessageParser;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.java_websocket.handshake.ServerHandshake;
@@ -14,6 +16,8 @@ public class WSClient extends org.java_websocket.client.WebSocketClient {
 
     private final Logger logger = LogManager.getLogger("WS");
 
+    @Getter
+    @Accessors(fluent = true)
     private final Configuration config;
 
     private final ChatAuthentication auth;
@@ -54,21 +58,22 @@ public class WSClient extends org.java_websocket.client.WebSocketClient {
         String[] messages = rawIrcMessage.split("\r\n"); // The IRC message may contain one or more messages.
         for (String message : messages) {
             IRCMessageParser.ParsedMessage parsedMessage = IRCMessageParser.parseMessage(message);
+            System.out.println(parsedMessage);
 
             if (parsedMessage != null) {
-                switch (parsedMessage.command.command) {
+                switch (parsedMessage.getCommand().getCommand()) {
                     case "PRIVMSG":
-                        commandHandler.execute(parsedMessage.command.botCommand, "", null);
+                        commandHandler.execute(parsedMessage.getCommand().getBotCommand(), "", null, this);
                         break;
                     case "PING":
-                        this.send(STR."PONG \{parsedMessage.parameters}");
+                        this.send(STR."PONG \{parsedMessage.getParameters()}");
                         break;
                     case "001":
                         this.send(STR."JOIN \{config.getChannel()}");
                         break;
                     case "JOIN":
 //                            this.send("PRIVMSG " + config.getChannel() + " :" + moveMessage);
-                        this.send(STR."PRIVMSG \{config.getChannel()} :Channel joint");
+                        this.send(STR."PRIVMSG #\{config.getChannel()} :Channel joint");
 
                         break;
                     case "PART":
@@ -76,12 +81,12 @@ public class WSClient extends org.java_websocket.client.WebSocketClient {
                         this.close();
                         break;
                     case "NOTICE":
-                        if ("Login authentication failed".equals(parsedMessage.parameters)) {
+                        if ("Login authentication failed".equals(parsedMessage.getParameters())) {
                             System.out.println(STR."Authentication failed; left \{config.getChannel()}");
 
                             this.send(STR."PART \{config.getChannel()}");
 
-                        } else if ("You don’t have permission to perform that action".equals(parsedMessage.parameters)) {
+                        } else if ("You don’t have permission to perform that action".equals(parsedMessage.getParameters())) {
                             System.out.println(STR."No permission. Check if the access token is still valid. Left \{config.getChannel()}");
                             send(STR."PART \{config.getChannel()}");
 
@@ -98,7 +103,9 @@ public class WSClient extends org.java_websocket.client.WebSocketClient {
 
     @Override
     public void onClose(int i, String s, boolean b) {
+        System.out.println("Disconnected");
         System.out.println(STR."i=\{i}, s=\{s} b=\{b}");
+        // TODO impl reconnection
     }
 
     @Override
