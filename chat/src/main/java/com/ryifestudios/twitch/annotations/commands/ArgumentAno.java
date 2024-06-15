27bb56087d3499a1470b036dbc0db94f3a4f0faf
Tiri <tiri0007@yahoo.com>
2024-06-16 00:56:16 +0200

    optional not required

--- chat/src/main/java/com/ryifestudios/twitch/annotations/commands/ArgumentAno.java
+++ chat/src/main/java/com/ryifestudios/twitch/annotations/commands/ArgumentAno.java
@@ -1,10 +1,6 @@
 package com.ryifestudios.twitch.annotations.commands;
 
 public @interface ArgumentAno {
-
     String name();
     String description();
-
-    boolean optional() default false;
-
 }
