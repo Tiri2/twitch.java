f5d514852f7df738ba2ea0675a58a16bc427f742
Tiri <tiri0007@yahoo.com>
2024-06-16 01:21:38 +0200

    user info added

--- chat/src/main/java/com/ryifestudios/twitch/models/User.java
+++ chat/src/main/java/com/ryifestudios/twitch/models/User.java
@@ -1,4 +1,27 @@
 package com.ryifestudios.twitch.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
 public class User {
+
+    private int userId;
+    private String displayName;
+    private String color;
+    private boolean mod;
+    private boolean subscriber;
+    private boolean turbo;
+    private int roomId;
+    private boolean firstMsg;
+
+    @Override
+    public String toString() {
+        return STR."User{userId=\{userId}, displayName='\{displayName}\{'\''}, color='\{color}\{'\''}, mod=\{mod}, subscriber=\{subscriber}, turbo=\{turbo}, roomId=\{roomId}, firstMsg=\{firstMsg}\{'}'}";
+    }
 }
