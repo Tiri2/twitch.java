aeb131a9300fe1e9c39ebf9580159c4d5add7c14
Tiri <tiri0007@yahoo.com>
2024-06-16 23:06:28 +0200

    web api added

---
+++ chat/src/main/java/com/ryifestudios/twitch/webapi/routes/GetCommands.java
@@ -0,0 +1,64 @@
+package com.ryifestudios.twitch.webapi.routes;
+
+import com.ryifestudios.twitch.commands.CommandHandler;
+import com.ryifestudios.twitch.commands.models.Argument;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class GetCommands implements Handler {
+
+    private final CommandHandler commandHandler;
+
+    public GetCommands(CommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
+    }
+
+    @Override
+    public void handle(@NotNull Context context) throws Exception {
+
+        JSONArray commands = new JSONArray();
+
+        System.out.println(commandHandler.commands());
+
+        commandHandler.commands().forEach((name, cmd) -> {
+            JSONObject js = new JSONObject();
+            js.put("name", name);
+            js.put("description", cmd.getDescription());
+
+            JSONArray subCmds = new JSONArray();
+
+            cmd.getSubCommands().forEach((sName, sCmd) -> {
+                JSONObject sJs = new JSONObject();
+                sJs.put("name", sName);
+                sJs.put("description", sCmd.getDescription());
+                JSONArray args = new JSONArray();
+
+                for (Argument argument : sCmd.getArguments()) {
+                    JSONObject argJson = new JSONObject();
+
+                    argJson.put("name", argument.getName());
+                    argJson.put("description", argument.getDescription());
+
+                    args.put(argJson);
+                }
+
+                subCmds.put(sJs);
+            });
+
+            js.put("subCommands", subCmds);
+            commands.put(js);
+            System.out.println(js);
+        });
+
+        System.out.println(commands);
+
+        context.json(commands.toString());
+
+    }
+
+
+
+}
