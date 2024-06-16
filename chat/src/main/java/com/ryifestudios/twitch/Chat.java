03c5d2e0f8e5945eabcb2ce635a0468474b2d9a3
Tiri <tiri0007@yahoo.com>
2024-06-16 16:58:25 +0200

    better logging

--- chat/src/main/java/com/ryifestudios/twitch/Chat.java
+++ chat/src/main/java/com/ryifestudios/twitch/Chat.java
@@ -4,7 +4,10 @@ package com.ryifestudios.twitch;
 import com.ryifestudios.twitch.commands.CommandHandler;
 import com.ryifestudios.twitch.configuration.AuthConfiguration;
 import com.ryifestudios.twitch.configuration.Configuration;
+import com.ryifestudios.twitch.events.EventHandler;
 import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Timer;
 
@@ -12,6 +15,10 @@ import java.util.Timer;
  *
  */
 public class Chat {
+
+
+    private final Logger logger = LogManager.getLogger();
+
 
     /**
      * Websocket client variable
@@ -25,16 +32,22 @@ public class Chat {
     @Getter
     private final CommandHandler commandHandler;
 
+    /**
+     * Everything about event
+     */
+    @Getter
+    private final EventHandler eventHandler;
+
     private final Timer timer;
 
     /**
-     *
-     * @param config authConfiguration which is used to store the required data like client Id or client Secret
-     * @param authConfig
+     * Constructor for {@link Chat}
+     * @param config specific Chat settings like which channel to connect and the nick name
+     * @param authConfig which is used to store the required data like client Id or client Secret
      */
     public Chat(Configuration config, AuthConfiguration authConfig) {
-
         this.commandHandler = new CommandHandler();
+        this.eventHandler = new EventHandler();
 
         timer = new Timer();
         client = new WSClient(config, commandHandler);
@@ -42,6 +55,8 @@ public class Chat {
         ChatAuthentication authentication = new ChatAuthentication(authConfig, client::connect);
 
         client.setAuth(authentication);
+
+        logger.info("chat started");
 
     }
 
