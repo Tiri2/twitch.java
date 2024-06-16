31f5272146f11bf0c8bfdd4b7a4ec2e19ff5b829
Tiri <tiri0007@yahoo.com>
2024-06-16 16:58:40 +0200

    generate user changed

--- chat/src/main/java/com/ryifestudios/twitch/commands/CommandContext.java
+++ chat/src/main/java/com/ryifestudios/twitch/commands/CommandContext.java
@@ -2,6 +2,7 @@ package com.ryifestudios.twitch.commands;
 
 import com.ryifestudios.twitch.WSClient;
 import com.ryifestudios.twitch.models.User;
+import lombok.Getter;
 
 import java.util.Map;
 
@@ -14,6 +15,15 @@ public class CommandContext {
     private final Map<String, Object> tags;
 
     /**
+     * -- GETTER --
+     *  Get a user to display his personal infos like display name, if he is mod and so on
+     *
+     * @return the user
+     */
+    @Getter
+    private final User user;
+
+    /**
      * Create a new Command Content
      * @param client the websocket client, to send messages
      * @param tags tags used for getting information like the message id
@@ -21,6 +31,12 @@ public class CommandContext {
     public CommandContext(WSClient client, Map<String, Object> tags) {
         this.client = client;
         this.tags = tags;
+
+        user = new User(Integer.parseInt(tags.get("user-id").toString()), tags.get("display-name").toString(), tags.get("color").toString(),
+                Boolean.parseBoolean(tags.get("mod").toString()),
+                Boolean.parseBoolean(tags.get("subscriber").toString()), Boolean.parseBoolean(tags.get("turbo").toString()),
+                Integer.parseInt(tags.get("room-id").toString()), Boolean.parseBoolean(tags.get("first-msg").toString()));
+
     }
 
     /**
@@ -37,17 +53,6 @@ public class CommandContext {
      */
     public void reply(String text){
         client.send(STR."@reply-parent-msg-id=\{tags.get("id")} PRIVMSG #\{client.config().getChannel()} :\{text}");
-    }
-
-    /**
-     * Get a user to display his personal infos like display name, if he is mod and so on
-     * @return
-     */
-    public User getUser(){
-        return new User(Integer.parseInt(tags.get("user-id").toString()), tags.get("display-name").toString(), tags.get("color").toString(),
-                Boolean.parseBoolean(tags.get("mod").toString()),
-                Boolean.parseBoolean(tags.get("subscriber").toString()), Boolean.parseBoolean(tags.get("turbo").toString()),
-                Integer.parseInt(tags.get("room-id").toString()), Boolean.parseBoolean(tags.get("first-msg").toString()));
     }
 
 }
